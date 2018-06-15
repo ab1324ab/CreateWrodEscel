ecec52a29aac9029265a3cc4f410e9709e0d87bc
hwk-t170 <ab1324ab@outlook.com>
2018-06-15 20:37:35 +0800

    更新下载1

--- src/main/java/com/myigou/tool/UpdateClient.java
+++ src/main/java/com/myigou/tool/UpdateClient.java
@@ -17,10 +17,10 @@ public class UpdateClient {
      * @param clientPanel
      */
     public static void updateInstall(JFrame clientPanel, String version) {
-        if ("error".equals(version)) {
+        if (version.indexOf("error") != -1) {
             JOptionPane.showMessageDialog(clientPanel, "当前管理员禁止使用 ! ", "提示", JOptionPane.QUESTION_MESSAGE);
             System.exit(0);
-        } else if (!WindowView.VERSION_THIS.equals(version) && !StringUtils.isEmpty(version)) {
+        } else if (!WindowView.VERSION_THIS.equals(version)) {
             JOptionPane.showMessageDialog(clientPanel, "当前版本有更新" + version, "提示", JOptionPane.WARNING_MESSAGE);
             // JFrame jFrameGenx=new JFrame("dd");
             // jFrameGenx.setSize(300,100);
